de9c8742fd2297caf9f5157b45f23ae412a20109
toyowata <toyomasa.watarai@gmail.com>
2019-02-08 14:35:19 +0900

    Add mbed_app.json

--- main.cpp
+++ main.cpp
@@ -3,34 +3,37 @@
 
 #define ADDR_MMA7660 (0x4C << 1)
 
-MMA7660FC acc(I2C_SDA, I2C_SCL, ADDR_MMA7660);
-Serial pc(USBTX, USBRX);
+#if defined(TARGET_WIO_3G) || defined (TARGET_WIO_BG96)
 DigitalOut GrovePower(GRO_POWR, 1);
-InterruptIn collision(D20);
-int hit = 0;
+#define COLISN_PIN D20
+#else
+#define COLISN_PIN D2
+#endif
+
+MMA7660FC acc(I2C_SDA, I2C_SCL, ADDR_MMA7660);
+InterruptIn collision(COLISN_PIN);
+
+int hits = 0;
 
 void func(void){
-    hit = 1;
+    hits++;
 }
 
 int main(void) {
     float x=0, y=0, z=0;
     acc.init();
-    pc.baud(115200);
     collision.mode(PullUp);
     collision.fall(&func);
-    pc.printf("*** sensor test start ***\n\n");
+
+    printf("*** sensor test start ***\n\n");
 
     while(1) {
-        if (hit == 1) {
-            pc.printf("*");
-            hit = 0;
-        }
         acc.read_Tilt(&x, &y, &z);
-        pc.printf("x: %2.2f degree \n", x);                    // Print the tilt orientation of the X axis
-        pc.printf("y: %2.2f degree \n", y);                    // Print the tilt orientation of the Y axis
-        pc.printf("z: %2.2f degree \n", z);                    // Print the tilt orientation of the Z axis
-        pc.printf("\n");
+        printf("hits: %d\n", hits);
+        printf("x: %6.2f degree\n", x);
+        printf("y: %6.2f degree\n", y);
+        printf("z: %6.2f degree\n", z);
+        printf("\n");
         wait(0.5);
     }
 }
